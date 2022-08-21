48ed6aac4be46a5b5d40ade28a58a6a5541eeb0a
ahasif <akasifbz@gmail.com>
2022-08-21 13:06:37 +0600

    new version is comeing

---
+++ asstes/js/custom.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+     $(".services-hero").owlCarousel({
+          margin: 10,
+          autoplay: true,
+          loop: true,
+          autoplayHoverPause: true,
+          autoplayTimeout: 3000,
+          smartSpeed: 3000,
+     });   
+
+
+});
